8ff4d2064dac117bae0339e85741eb24fdfd7368
assess15 <assess126@gmail.com>
2020-03-13 22:44:41 +0800

    add files

---
+++ testcode/src/com/assess15/algorithm/sort/BubbleSort/BubbleSort.java
@@ -0,0 +1,33 @@
+package com.assess15.algorithm.sort.BubbleSort;
+
+import java.util.Arrays;
+
+/**
+ * 未优化
+ */
+
+public class BubbleSort {
+
+    private static void sort(int[] array) {
+        if (array.length <= 1) {
+            return;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{5, 8, 6, 3, 9, 2, 1, 7};
+        sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+}
---
+++ testcode/src/com/assess15/algorithm/sort/InsertionSort/InsertionSort.java
@@ -0,0 +1,12 @@
+package com.assess15.algorithm.sort.InsertionSort;
+
+public class InsertionSort {
+
+    public static void sort(int[] array) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
