d6220b857df6e428f6422131eec6d8081d16d03c
assess15 <assess15@126.com>
2020-05-07 15:29:10 +0800

    add 234

---
+++ testcode/src/com/assess15/algorithm/linkedList/Palindrome_Linked_List/Solution1.java
@@ -0,0 +1,25 @@
+package com.assess15.algorithm.linkedList.Palindrome_Linked_List;
+
+import java.util.Stack;
+
+/**
+ * 解法:利用额外的空间 Stack ,利用栈的先进后出,判断先进后出顺序是否一致
+ */
+
+class Solution1 {
+    public boolean isPalindrome(ListNode head) {
+        Stack<ListNode> stack = new Stack<ListNode>();
+        ListNode curr = head;
+        while (curr != null) {
+            stack.push(curr);
+            curr = curr.next;
+        }
+        while (head != null) {
+            if (head.val != stack.pop().val) {
+                return false;
+            }
+            head = head.next;
+        }
+        return true;
+    }
+}
