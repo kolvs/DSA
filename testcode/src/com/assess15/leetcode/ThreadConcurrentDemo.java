41ab5e2fea9407123fe75691d2c2df5125724123
assess15 <assess15@126.com>
2020-08-10 09:38:05 +0800

    update fils

---
+++ testcode/src/com/assess15/leetcode/ThreadConcurrentDemo.java
@@ -0,0 +1,42 @@
+package com.assess15.leetcode;
+
+/**
+ * 用三个线程，顺序打印字母A-Z，输出结果是1A、2B、3C、1D 2E...
+ */
+
+public class ThreadConcurrentDemo {
+
+    private static char c = 'A';
+    private static int i = 0;
+
+    public static void main(String[] args) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                synchronized (this) {
+                    try {
+                        int threadId = Integer.parseInt(Thread.currentThread().getName());
+                        while (i < 26) {
+                            if (i % 3 == threadId - 1) {
+                                System.out.println(threadId + "" + (char) c++);
+                                i++;
+                                notifyAll();
+                            } else {
+                                wait();
+                            }
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        Thread t1 = new Thread(runnable, "1");
+        Thread t2 = new Thread(runnable, "2");
+        Thread t3 = new Thread(runnable, "3");
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
