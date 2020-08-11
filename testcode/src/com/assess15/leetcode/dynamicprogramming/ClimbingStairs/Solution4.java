8893ba7e6a6efae4c86f2997c66018701cc4941f
assess15 <assess15@126.com>
2020-08-11 15:22:29 +0800

    add 70

---
+++ testcode/src/com/assess15/leetcode/dynamicprogramming/ClimbingStairs/Solution4.java
@@ -0,0 +1,16 @@
+package com.assess15.leetcode.dynamicprogramming.ClimbingStairs;
+
+/**
+ * 公式法
+ * <p>
+ * Fn = 1 / 5 [(1+ √5 / 2)^n - (1 - √5 / 2)^n]
+ *
+ * 时间复杂度：O(logn)
+ */
+class Solution4 {
+    public int climbStairs(int n) {
+        double sqrt5 = Math.sqrt(5);
+        double fib = Math.pow((1 + sqrt5) / 2, n + 1) - Math.pow((1 - sqrt5) / 2, n + 1);
+        return (int) (fib / sqrt5);
+    }
+}
