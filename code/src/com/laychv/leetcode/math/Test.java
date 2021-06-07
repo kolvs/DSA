6a1124215095c94dc15e2eb7e8a46fe9e810be14
lay <assess15@126.com>
2021-06-07 19:38:09 +0800

    136

---
+++ code/src/com/laychv/leetcode/math/Test.java
@@ -0,0 +1,19 @@
+package com.laychv.leetcode.math;
+
+public class Test {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 2;
+        int c = 1;
+        int d = 3;
+        System.out.println(a ^ b ^ c ^ d);
+        String sa = Integer.toBinaryString(a);
+        String sb = Integer.toBinaryString(b);
+        String sc = Integer.toBinaryString(c);
+        String sd = Integer.toBinaryString(d);
+        System.out.println(sa);
+        System.out.println(sb);
+        System.out.println(sc);
+        System.out.println(sd);
+    }
+}
