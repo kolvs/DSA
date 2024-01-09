c2476ebeea54a02a88af2563c2296af6dd020d05
lay <laychv@gmail.com>
2024-01-09 21:53:28 +0800

    209

--- src/main/java/com/laychv/leetcode/_209_MinimumSizeSubarraySum.java
+++ src/main/java/com/laychv/leetcode/_209_MinimumSizeSubarraySum.java
@@ -29,6 +29,7 @@ public class _209_MinimumSizeSubarraySum {
 
     public static void main(String[] args) {
         System.out.println("2:" + minSubArrayLen(7, new int[]{2, 3, 1, 2, 4, 3}));
+        System.out.println("2:" + minSubArrayLen2(7, new int[]{2, 3, 1, 2, 4, 3}));
     }
 
     /**
@@ -44,6 +45,26 @@ public class _209_MinimumSizeSubarraySum {
             while (sum >= target) {
                 res = Math.min(res, right - left + 1);
                 sum -= nums[left++];
+            }
+        }
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+
+    /**
+     * 暴力
+     * leetcode error
+     */
+    public static int minSubArrayLen2(int target, int[] nums) {
+        int res = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum >= target) {
+                    res = Math.min(res, j - i + 1);
+                    break;
+                }
             }
         }
         return res == Integer.MAX_VALUE ? 0 : res;
